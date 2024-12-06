142db68e4b14cce3e3ac413a8af9457f3cae37de
Hungpham2k4 <22.phamviethung.12a12@gmail.com>
2024-12-06 17:35:53 +0700

    fix name fanpage-service

---
+++ backend/fanpages_service/src/test/java/com/hungdev/fanpages_service/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungdev.fanpages_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
